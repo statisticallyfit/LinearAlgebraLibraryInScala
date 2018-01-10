08a5cecdf0d0bf3fe04cc616d3f5a54ea5c7b412
statisticallyfit <statisticallyfit@gmail.com>
2018-01-10 14:08:57 -0500

    save working copy: going to get interoperability by reframing Number[L, H] and passing this to RealNumber[L, H] so that we can have def +(other: R): N in the typeclass

--- src/main/scala/linalg/numeric/complextest.scala
+++ src/main/scala/linalg/numeric/complextest.scala
@@ -1,24 +1,23 @@
-package linalg.numeric
-
-
-import linalg.theory._
-import linalg.util.ImplicitConversions._
-import org.apache.commons.lang3.math.Fraction
-import spire.math.{Complex, Rational}
-import spire.math.Complex._
-
-import scala.language.implicitConversions
-
-
-
-
-object complextest extends App {
-
-     val j: Complex[Int] = Complex.i[Int]
-
-     val a: Complex[Int] = Complex(2,3) + 5
-     val b: Complex[Rational] = Complex(Rational(2,3), Rational(4,5))
-
-     // can't do this with spire unfortunately, so must build my own if wanting to be fancy.
-     //val c: Complex[Int] = 3 + 3*j
-}
+//package linalg.numeric
+//
+//
+//import spire.math.{Complex, Rational}
+//import scala.language.implicitConversions
+//
+//
+//
+//
+//object complextest extends App {
+//
+//     val j: Complex[Int] = Complex.i[Int]
+//
+//     val a: Complex[Int] = Complex(2,3) + 5
+//     val b: Complex[Rational] = Complex(Rational(2,3), Rational(4,5))
+//
+//     println(a)
+//     println(b)
+//     println(Complex(2,3).eq)
+//
+//     // can't do this with spire unfortunately, so must build my own if wanting to be fancy.
+//     //val c: Complex[Int] = 3 + 3*j
+//}
