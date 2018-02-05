160680d1fb1fa7e3346a02d7e5902d3da219dc36
statisticallyfit <statisticallyfit@gmail.com>
2018-02-05 13:56:32 -0500

    figure out subspace

--- src/main/scala/linalg/theory/space/Subspace.scala
+++ src/main/scala/linalg/theory/space/Subspace.scala
@@ -1,6 +1,6 @@
 package linalg.theory.space
 
-
+import linalg.theory._
 
 /**
   * A non-empty subset S of vector space V is a subspace of V if it also
@@ -8,6 +8,8 @@ package linalg.theory.space
   */
 trait Subspace[S, F] extends VectorSpace[S, F] {
 
+     //get a subspace of vecspace V (by getting a spanning set S)
+     //todo def subspace(implicit ev: Span[S, F]): S = ev.span
      def isSubsetOf(subset: S, parent: S): Boolean
      def isSubspaceOf(subspace: S, parent: S): Boolean
 }
