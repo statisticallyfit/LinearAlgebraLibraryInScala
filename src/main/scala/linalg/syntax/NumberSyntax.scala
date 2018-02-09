8bdcfd062b8027ec99db0483ed29150e56c558f1
statisticallyfit <statisticallyfit@gmail.com>
2018-02-09 14:08:43 -0500

    still trying to make it

--- src/main/scala/linalg/syntax/NumberSyntax.scala
+++ src/main/scala/linalg/syntax/NumberSyntax.scala
@@ -1,7 +1,10 @@
 package linalg.syntax
 
 import linalg.numeric._
+import linalg.theory._
 
+import scala.language.implicitConversions
+import scala.language.higherKinds
 /**
   *
   */
@@ -22,6 +25,45 @@ object NumberSyntax {
           def toDouble: Double = number.doubleValue(current)
           def toInt: Int = number.doubleValue(current).toInt // todo check this can be chopped off!
 
+
+          private val comp: Compare[N] = number.numberIsComparable
+
+          def :==:(other: N): Boolean = comp.equal(current, other)
+          def !==(other: N): Boolean = ! comp.equal(current, other)
+          def <(other: N): Boolean = comp.lessThan(current, other)
+          def >(other: N): Boolean = comp.greaterThan(current, other)
+          def <=(other: N): Boolean = comp.lessThanOrEqual(current, other)
+          def >=(other: N): Boolean = comp.greaterThanOrEqual(current, other)
+
+
+          /*private val root: Root0[Number[N], N] = number.numberHasRoot
+
+          def ^(exp: N): Number[N] = root.power(current, exp)
+          def sqrt(): Number[N] = root.squareRoot(current)
+          def nRoot(n: N): Number[N] = root.nRoot(current, n)*/
      }
+
+     implicit class RootOps[R: Root: Field](base: R)(implicit root: Root[R]){
+
+          def ^(exp: R): R = root.power(base, exp)
+          def sqrt(): R = root.squareRoot(base)
+          def nRoot(n: R): R = root.nRoot(base, n)
+     }
+
+     /*implicit class RootBaseOps[N: Number](base: Number[N])(implicit root: Root0[Number[N], N]){
+
+          def ^(exp: N): Number[N] = root.power(base, exp)
+          def sqrt(): Number[N] = root.squareRoot(base)
+          def nRoot(n: N): Number[N] = root.nRoot(base, n)
+     }*/
+     implicit class RootBaseOps[H[_], L: Field](base: H[L])(implicit root: Root0[H[L], L],
+                                                            numH: Number[H[L]],
+                                                            numL: Number[L]){
+
+          def ^(exp: L): H[L] = root.power(base, exp)
+          def sqrt(): H[L] = root.squareRoot(base)
+          def nRoot(n: L): H[L] = root.nRoot(base, n)
+     }
+     Complex(1,2).nRoot(2)
 
 }
